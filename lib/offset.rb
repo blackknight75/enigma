9ecc0c87599f9ffb52bd74a20db40048b15c64d5
James Anderson <james.p.anderson16@gmail.com>
2016-12-11 22:53:14 -0700

    added function combining key and offset

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,15 +1,34 @@
+require "pry"
+
 class Offset
 
-attr_reader :date
+attr_reader :date, :keygen
   def initialize(date)
     @today = Time.now.strftime("%D").delete("/").to_i
-    @date = @today
+    @date = date
+    @keygen = KeyGenerator.new
+    @date_offset = []
+    @key_array = []
   end
 
   def date_offsets
-    date_offset = ((@date ** 2) % 10000).to_s.chars.map(&:to_i)
+    @date_offset = ((@date ** 2) % 10000).to_s.chars.map(&:to_i)
   end
 
-  # def zip
+  def key_array
+    @key_array = []
+    (@keygen.key.length - 1).times do |index|
+      @key_array << ((@keygen.key[index] + @keygen.key[index + 1]).to_i)# + (@date_offset[index])
+    end
+  end
+
+  def key_join
+    result = []
+    @key_array.each_with_index do |num, index|
+      result << num + @date_offset[index]
+    end
+    result
+    binding.pry
+  end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset'
+require './lib/key_generator'
 require "pry"
 
 class OffsetTest < Minitest::Test
@@ -11,7 +12,20 @@ class OffsetTest < Minitest::Test
 # binding.pry
   end
 
-  # def test_date_key
-  # assert_equal
+  def test_it_runs_keygen_and_is_random
+    offset = Offset.new(121016)
+    assert_equal 5, offset.keygen.key.length
+    refute_equal 94827, offset.keygen.key.to_i
+    refute_equal 12345, offset.keygen.key.to_i
+    refute_equal 98765, offset.keygen.key.to_i
+  end
+
+  def test_can_join_key_and_offset
+    offset = Offset.new(121016)
+    offset.date_offsets
+    offset.key_array
+    assert_equal 0, offset.key_join
+  end
+
 
 end
